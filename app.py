522d54d112491e8a3aee6ffa45975aae620bb538
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2021-09-19 02:07:48 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 
 from flask import Flask, render_template, request
 import nowLive
-import iptvhdFcn
+#import iptvhdFcn
 #import model
 import json
 import base64
@@ -11,6 +11,19 @@ import time
 import os
 port = int(os.environ.get("PORT", 5000))	
 PORT_NUMBER = port
+
+listadecanales=''
+def readList():
+	global listadecanales
+	nowLive.updateList()
+	data=open('templates/info.m3u')
+	listadecanales=data.read()
+	data.close()
+	for lista in os.listdir('templates/listas'):
+		data=open('templates/listas/'+lista,encoding="utf8")
+		listadecanales=listadecanales+'\n'+data.read()
+		data.close()
+	print (listadecanales)
 
 app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
@@ -22,22 +35,21 @@ def root():
 def index():
 	return render_template("index.html")
 
-
+listaUsuarios=[]
 @app.route("/listaiptv")
 def listaiptv():
 	global lasUpdate
-	print(time.time()-lasUpdate)
-	if time.time()-lasUpdate>10000:
+	try:
+		print( ip_address = request.headers['X-Forwarded-For'])
+	except:
+		pass
+	if time.time()-lasUpdate>18000:
+		#nowLive.updateList()
+		#iptvhdFcn.getChannels()
 		lasUpdate=time.time()
-		nowLive.updateList()
-		iptvhdFcn.getChannels()
-		
-	return render_template("leoList.m3u")
-@app.route("/update")
-def update():
-	nowLive.updateList()
-	iptvhdFcn.getChannels()
-	return "ok"
+		readList()
+	return listadecanales
+readList()
 lasUpdate=time.time()
 if __name__ == "__main__":
 	app.run(host='0.0.0.0',port=PORT_NUMBER,debug = True)
