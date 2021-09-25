0887d4765fcc4be9f616f5c82e6a5aec5bf6cd4d
tvliveapp <61073350+tvliveapp@users.noreply.github.com>
2021-09-25 03:03:41 -0500

    Update app.py

--- app.py
+++ app.py
@@ -58,11 +58,11 @@ app = Flask(__name__)
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 @app.route("/")
 def root():
-	return render_template("index.html")
+	return render_template("test.html")
 
 @app.route("/index")
 def index():
-	return render_template("index.html")
+	return render_template("test.html")
 
 listaUsuarios=[]
 @app.route("/listaiptv")
